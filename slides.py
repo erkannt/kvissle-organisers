0ec0bfcab3edb22a0fd21d52f29a7db41953c13d
Daniel Haarhoff <daniel@rknt.de>
2024-04-07 21:02:58 +0100

    Production ready half and quarter quarter slides

--- slides.py
+++ slides.py
@@ -1,7 +1,6 @@
 # %%
 from build123d import *
 from ocp_vscode import *
-from copy import copy
 
 reset_show()
 
@@ -11,9 +10,9 @@ height = 40
 magnet_diam = 6.1
 magnet_depth = 6
 magnet_hole_inset = 4.2
-mouse_ear_diam=12
-mouse_ear_thickness=0.4
-mouse_ear_overlap=1.5
+mouse_ear_diam = 5
+mouse_ear_thickness = 0.4
+mouse_ear_overlap = 1.5
 wall = 4
 outer_corner_fillet = 2
 inner_corner_fillet = 10
@@ -31,9 +30,9 @@ sketch = (
     + Pos(width - wall, 0) * full_depth
 )
 slide_center = sketch.bounding_box().center()
-outer_corners = sketch.vertices().sort_by_distance(slide_center)[-4:]
+slide_outer_corners = sketch.vertices().sort_by_distance(slide_center)[-4:]
 sketch = fillet(
-    outer_corners,
+    slide_outer_corners,
     radius=outer_corner_fillet,
 )
 sketch = sketch + Pos(width / 2 - wall / 2, 0) * full_depth
@@ -58,27 +57,39 @@ magnet_holes = [
         height=magnet_depth * 2,
         align=[Align.CENTER, Align.CENTER, Align.MIN],
     )
-    for pos in outer_corners
+    for pos in slide_outer_corners
 ]
 
-mouse_ear_shift=(mouse_ear_diam/2-mouse_ear_overlap)/1.4
-mouse_ears = [
-    Plane.XY
-    * Pos(
-        pos.X + (-mouse_ear_shift if pos.X < slide_center.X else mouse_ear_shift),
-        pos.Y + (-mouse_ear_shift if pos.Y < slide_center.Y else mouse_ear_shift),
-        0,
-    )
-    * Cylinder(
-        radius=mouse_ear_diam / 2,
-        height=mouse_ear_thickness,
-        align=[Align.CENTER, Align.CENTER, Align.MIN],
-    )
-    for pos in outer_corners
-]
+slide = slide - magnet_holes
 
-slide = slide - magnet_holes + mouse_ears
-show_object(slide)
-slide.export_stl("slide-quarter-quarter-half.stl")
+def corner_mouse_ears(shape):
+    center = shape.bounding_box().center()
+    outer_corners = Solid.from_bounding_box(shape.bounding_box()).faces().group_by(Axis.Z)[0].vertices()
+    print(outer_corners)
+    mouse_ear_shift = (mouse_ear_diam / 2 - mouse_ear_overlap) / 1.4
+    mouse_ears = [
+        Plane.XY
+        * Pos(
+            pos.X + (-mouse_ear_shift if pos.X < center.X else mouse_ear_shift),
+            pos.Y + (-mouse_ear_shift if pos.Y < center.Y else mouse_ear_shift),
+            0,
+        )
+        * Cylinder(
+            radius=mouse_ear_diam / 2,
+            height=mouse_ear_thickness,
+            align=[Align.CENTER, Align.CENTER, Align.MIN],
+        )
+        for pos in outer_corners
+    ]
+    return mouse_ears
+
+right = Pos(50,0,0) * split(slide, Plane.YZ.offset(width / 2), keep=Keep.TOP)
+left = split(slide, Plane.YZ.offset(width / 2), keep=Keep.BOTTOM)
+right = right + corner_mouse_ears(right)
+left = left + corner_mouse_ears(left)
+show_object(left)
+show_object(right)
+left.export_stl("slide-quarter-quarter-half--left.stl")
+right.export_stl("slide-quarter-quarter-half--right.stl")
 
 # %%
