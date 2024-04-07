334376b9323e54490194f5718e10867d5bb4dac1
Daniel Haarhoff <daniel@rknt.de>
2024-04-07 21:13:23 +0100

    Refactor to prepare for more variants

--- slides.py
+++ slides.py
@@ -4,69 +4,29 @@ from ocp_vscode import *
 
 reset_show()
 
-depth = 245
-width = 303
-height = 40
-magnet_diam = 6.1
-magnet_depth = 6
-magnet_hole_inset = 4.2
-mouse_ear_diam = 5
-mouse_ear_thickness = 0.4
-mouse_ear_overlap = 1.5
-wall = 4
-outer_corner_fillet = 2
-inner_corner_fillet = 10
-top_edge_fillet = wall / 3
+DEPTH = 245
+WIDTH = 303
+HEIGHT = 40
+MAGNET_DIAM = 6.1
+MAGNET_DEPTH = 6
+MAGNET_HOLE_INSET = 4.2
+MOUSE_EAR_DIAM = 5
+MOUSE_EAR_THICKNESS = 0.4
+MOUSE_EAR_OVERLAP = 1.5
+WALL = 4
+OUTER_CORNER_FILLET = 2
+INNER_CORNER_FILLET = 10
 
-quarter_wall_loc = (0, width / 2 - (width - 2 * wall) / 4)
-
-full_width = Rectangle(width=width, height=wall, align=Align.MIN)
-full_depth = Rectangle(width=wall, height=depth, align=Align.MIN)
-
-sketch = (
-    full_width
-    + Pos(0, depth - wall) * full_width
-    + full_depth
-    + Pos(width - wall, 0) * full_depth
-)
-slide_center = sketch.bounding_box().center()
-slide_outer_corners = sketch.vertices().sort_by_distance(slide_center)[-4:]
-sketch = fillet(
-    slide_outer_corners,
-    radius=outer_corner_fillet,
-)
-sketch = sketch + Pos(width / 2 - wall / 2, 0) * full_depth
-sketch = sketch + Pos(width / 4 - wall / 2, 0) * full_depth
-sketch = fillet(sketch.vertices(), radius=inner_corner_fillet)
-sacrificial_helper_to_avoid_fillet_issue = extrude(sketch, amount=-top_edge_fillet)
-slide = extrude(sketch, amount=height) + sacrificial_helper_to_avoid_fillet_issue
-slide = (
-    fillet(slide.edges(), radius=top_edge_fillet)
-    - sacrificial_helper_to_avoid_fillet_issue
-)
-
-magnet_holes = [
-    Plane.XY
-    * Pos(
-        pos.X + (magnet_hole_inset if pos.X < slide_center.X else -magnet_hole_inset),
-        pos.Y + (magnet_hole_inset if pos.Y < slide_center.Y else -magnet_hole_inset),
-        0,
-    )
-    * Cylinder(
-        radius=magnet_diam / 2,
-        height=magnet_depth * 2,
-        align=[Align.CENTER, Align.CENTER, Align.MIN],
-    )
-    for pos in slide_outer_corners
-]
-
-slide = slide - magnet_holes
 
 def corner_mouse_ears(shape):
     center = shape.bounding_box().center()
-    outer_corners = Solid.from_bounding_box(shape.bounding_box()).faces().group_by(Axis.Z)[0].vertices()
-    print(outer_corners)
-    mouse_ear_shift = (mouse_ear_diam / 2 - mouse_ear_overlap) / 1.4
+    outer_corners = (
+        Solid.from_bounding_box(shape.bounding_box())
+        .faces()
+        .group_by(Axis.Z)[0]
+        .vertices()
+    )
+    mouse_ear_shift = (MOUSE_EAR_DIAM / 2 - MOUSE_EAR_OVERLAP) / 1.4
     mouse_ears = [
         Plane.XY
         * Pos(
@@ -75,16 +35,65 @@ def corner_mouse_ears(shape):
             0,
         )
         * Cylinder(
-            radius=mouse_ear_diam / 2,
-            height=mouse_ear_thickness,
+            radius=MOUSE_EAR_DIAM / 2,
+            height=MOUSE_EAR_THICKNESS,
             align=[Align.CENTER, Align.CENTER, Align.MIN],
         )
         for pos in outer_corners
     ]
     return mouse_ears
 
-right = Pos(50,0,0) * split(slide, Plane.YZ.offset(width / 2), keep=Keep.TOP)
-left = split(slide, Plane.YZ.offset(width / 2), keep=Keep.BOTTOM)
+
+def extrude_and_fillet(sk):
+    top_edge_fillet = WALL / 3
+    sacrificial_helper_to_avoid_fillet_issue = extrude(sk, amount=-top_edge_fillet)
+    body = extrude(sk, amount=HEIGHT) + sacrificial_helper_to_avoid_fillet_issue
+    body = (
+        fillet(body.edges(), radius=top_edge_fillet)
+        - sacrificial_helper_to_avoid_fillet_issue
+    )
+    return body
+
+
+full_width = Rectangle(width=WIDTH, height=WALL, align=Align.MIN)
+full_depth = Rectangle(width=WALL, height=DEPTH, align=Align.MIN)
+
+base_sketch = (
+    full_width
+    + Pos(0, DEPTH - WALL) * full_width
+    + full_depth
+    + Pos(WIDTH - WALL, 0) * full_depth
+)
+slide_center = base_sketch.bounding_box().center()
+slide_outer_corners = base_sketch.vertices().sort_by_distance(slide_center)[-4:]
+base_sketch = fillet(
+    slide_outer_corners,
+    radius=OUTER_CORNER_FILLET,
+)
+
+magnet_holes = [
+    Plane.XY
+    * Pos(
+        pos.X + (MAGNET_HOLE_INSET if pos.X < slide_center.X else -MAGNET_HOLE_INSET),
+        pos.Y + (MAGNET_HOLE_INSET if pos.Y < slide_center.Y else -MAGNET_HOLE_INSET),
+        0,
+    )
+    * Cylinder(
+        radius=MAGNET_DIAM / 2,
+        height=MAGNET_DEPTH * 2,
+        align=[Align.CENTER, Align.CENTER, Align.MIN],
+    )
+    for pos in slide_outer_corners
+]
+
+sketch1 = base_sketch
+sketch1 = sketch1 + Pos(WIDTH / 2 - WALL / 2, 0) * full_depth
+sketch1 = sketch1 + Pos(WIDTH / 4 - WALL / 2, 0) * full_depth
+sketch1 = fillet(sketch1.vertices(), radius=INNER_CORNER_FILLET)
+
+slide1 = extrude_and_fillet(sketch1) - magnet_holes
+right = Pos(50, 0, 0) * split(slide1, Plane.YZ.offset(WIDTH / 2), keep=Keep.TOP)
+left = split(slide1, Plane.YZ.offset(WIDTH / 2), keep=Keep.BOTTOM)
 right = right + corner_mouse_ears(right)
 left = left + corner_mouse_ears(left)
 show_object(left)
